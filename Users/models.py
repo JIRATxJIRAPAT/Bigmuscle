da4be948763d71bc4d60c16dec763b6c93ba5895
Jirat Sukphalanon <6210612633@cn330>
2021-10-17 22:11:58 +0700

    models v1

--- Users/models.py
+++ Users/models.py
@@ -3,27 +3,30 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Trainer(models.Model):
-    tr_id = models.IntegerField(primary_key=True)
+    trainer_id = models.PositiveIntegerField(primary_key=True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,blank = True)
     gender = models.CharField(max_length = 20)
     specialist = models.CharField(max_length = 50)
-    user = models.OneToOneField(User,on_delete=models.CASCADE,blank = True)
+    
+    
     def __str__(self):
-        return f"{self.tr_id}"
+        return f"{self.trainer_id}"
 
 class Course(models.Model):
-    name = models.CharField(max_length = 20 , primary_key = True)
+    course_id = models.PositiveIntegerField(primary_key=True)
+    name = models.CharField(max_length = 20)
     info = models.CharField(max_length = 500)
-    teach = models.ManyToManyField(Trainer ,related_name="teach")
+    teach = models.ManyToManyField(Trainer ,related_name="teacher")
     
     def __str__(self):
         return f"{self.name} "
     
 class Customer(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,blank = True)
-    owned = models.ForeignKey(Course,on_delete=models.CASCADE,related_name="member") #null
+    owned = models.ForeignKey(Course,on_delete=models.CASCADE,blank = True,related_name="study",null = True) #null
     weight = models.PositiveIntegerField(default = 0)
     height = models.PositiveIntegerField(default = 0)
+    trainer = models.ForeignKey(Trainer,on_delete=models.CASCADE,related_name="my_trainer",null = True,blank = True)
     
-    trainer = models.ForeignKey(Trainer,on_delete=models.CASCADE,related_name="member")
     def __str__(self):
-        return f"{self.user}"
+        return f"{self.user} {self.id}"
