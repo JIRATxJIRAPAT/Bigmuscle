460dba8de624b75b1d3f10c667a0c50b99db42ca
Jirat Sukphalanon <6210612633@cn330>
2021-10-29 20:47:27 +0700

    update signup

--- Users/forms.py
+++ Users/forms.py
@@ -9,4 +9,4 @@ class CreateUserForm(UserCreationForm):
 	
     class Meta:
 	        model = User
-	        fields = ['username', 'email', 'password1', 'password2']
+	        fields = ['username', 'email', 'password1', 'password2','first_name','last_name']
