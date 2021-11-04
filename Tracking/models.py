8042189f91bd66d8d3c5e8f278a4a077d1bd1137
Jirat Sukphalanon <6210612633@cn330>
2021-11-04 22:47:35 +0700

    ver2

--- Tracking/models.py
+++ Tracking/models.py
@@ -14,17 +14,17 @@ class Workout(models.Model):
     reps = models.PositiveIntegerField()
     sets = models.PositiveIntegerField()
     status = models.BooleanField(default=False)
-    exercise = models.OneToOneField(Exercise, on_delete=models.CASCADE, blank=True)
+    exercise = models.ForeignKey(Exercise,on_delete=models.CASCADE,null=True)
     created_by = models.ForeignKey('Users.Trainer', on_delete=models.CASCADE)
-    
+    owner = models.ForeignKey('Users.Customer',on_delete=models.CASCADE,null=True)
     def __str__(self):
-        return f"{self.exercise} in {self.date}"
+        return f"{self.exercise} in {self.date} {self.owner}"
 
 
 class Program(models.Model):
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
     owner = models.ForeignKey('Users.Customer',on_delete=models.CASCADE)
-    objective = models.ManyToManyField(Workout ,related_name="workout",blank = True)
+    objective = models.ManyToManyField(Workout,related_name="workout")
     def __str__(self):
-        return f"{self.start_date}"
+        return f"{self.owner} {self.start_date}"
